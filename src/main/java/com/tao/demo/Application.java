f6a0a562e15901767ef2b3e89e4c8b557b491eef
zhangtao <278766377@qq.com>
2020-03-15 19:17:29 +0800

    统一返回格式

--- src/main/java/com/tao/demo/Application.java
+++ src/main/java/com/tao/demo/Application.java
@@ -1,6 +1,7 @@
 package com.tao.demo;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.tao.frameworks.base.spring.RequestMappingHandlerAdapterModify;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,5 +20,15 @@ public class Application {
         PaginationInterceptor page = new PaginationInterceptor();
         return page;
     }
+
+    @Bean
+    public RequestMappingHandlerAdapterModify requestMappingHandlerAdapterModify() {
+        return new RequestMappingHandlerAdapterModify();
+    }
+
+//    @Bean
+//    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+//        return factory -> factory.setContextPath("/keeper");
+//    }
 
 }
